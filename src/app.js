95c6d620df4d64c5730f9290ec3678cab663747a
mbowdre <bowdremaya@gmail.com>
2023-08-09 06:24:44 -0500

    add temperature api
    added temp, humidity, wind speed, and geolocation

--- src/app.js
+++ src/app.js
@@ -37,3 +37,60 @@ function searchCity(city) {
 
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 }
+function changeFarenheit(event) {
+  event.preventDefault();
+  let temp = document.querySelector("span.temp");
+  temp.innerHTML = `66`;
+}
+let farenheit = document.querySelector("#farenheit");
+farenheit.addEventListener("click", changeFarenheit);
+
+function changeCelcius(event) {
+  event.preventDefault();
+  let celTemp = document.querySelector("span.temp");
+  celTemp.innerHTML = `19`;
+}
+let celcius = document.querySelector("#celcius");
+celcius.addEventListener("click", changeCelcius);
+
+function showTemperature(response) {
+  console.log(response.data);
+  let city = response.data.name;
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${city}`;
+
+  document.querySelector("span.temp").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
+    response.data.wind.speed
+  )}km/h`;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function currentTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector("span.temp");
+  currentTemp.innerHTML = `${temperature}`;
+}
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "60e9b8e93f7104c20384f8e74ed8be82";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(currentTemperature);
+}
+
+function displayCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocation = document.querySelector("#current");
+currentLocation.addEventListener("click", displayCurrentLocation);
