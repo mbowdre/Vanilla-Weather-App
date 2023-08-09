eb50c5b953796b4bbfd0f1d1658c0d5de0b2243d
mbowdre <bowdremaya@gmail.com>
2023-08-09 06:15:18 -0500

    Added city search api

--- src/app.js
+++ src/app.js
@@ -21,3 +21,19 @@ if (minutes < 10) {
 let day = days[now.getDay()];
 
 dates.innerHTML = `${day} ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-city");
+  searchCity(cityInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function searchCity(city) {
+  let apiKey = "60e9b8e93f7104c20384f8e74ed8be82";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
